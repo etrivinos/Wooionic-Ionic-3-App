ca5b7518bb959f9ebd57983409c35911d80787b9
Edwin Alberto Triviños Ramírez <Edwin Alberto Trivios Ramrez>
2018-04-06 03:56:42 -0500

    Displaying Product Reviews

--- src/pages/product-details/product-details.ts
+++ src/pages/product-details/product-details.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import * as WC from 'woocommerce-api';
+
 @IonicPage()
 @Component({
   selector: 'page-product-details',
@@ -9,12 +11,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class ProductDetailsPage {
 
 	product: any;
+  WooCommerce: any; 
+  reviews: any[] = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   	this.product = this.navParams.get('product');
 
-  	console.log('this.product');
-  	console.log(this.product);
+    this.WooCommerce = WC({
+      url:            "http://edwintrivinos.com/cusstom_apps/woo_commerce_ionic_3/",
+      consumerKey:    "ck_8f07070deceadfc7cbaffdb425b5621ef91e9956",
+      consumerSecret: "cs_732111e679d2bd8563bec6c03d670a56680a26d2"
+    });
+
+  	this.WooCommerce.getAsync("products/" + this.product.id + "/reviews")
+      .then((data) => {
+        this.reviews = JSON.parse(data.body).product_reviews;
+        console.log('this.reviews');
+        console.log(this.reviews);
+      }, (error) => {
+        console.log(error);
+      });
   }
 
   ionViewDidLoad() {
