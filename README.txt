0e20a81780578a9805f170196d0ef17e978764dd
Edwin Alberto Triviños Ramírez <Edwin Alberto Trivios Ramrez>
2018-04-06 01:48:39 -0500

    Infinite scrolling in home page

--- README.txt
+++ README.txt
@@ -1,2 +1,9 @@
 WooCommerce Dummy Data:
 http://plugins.svn.wordpress.org/woocommerce/tags/2.6.4/dummy-data/dummy-data.xml?dl=1
+
+npm install woocommerce-api --save
+
+==================================================================================================================
+ALLOW CONTROL ORIGIN:
+==================================================================================================================
+https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi/related
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,80 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides, ToastController } from 'ionic-angular';
+
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  WooCommerce: any;
+  products: any;
+  moreProducts: any;
 
-  constructor(public navCtrl: NavController) {
+  page: number;
 
+  @ViewChild('productSlides') productSlides: Slides;
+
+  constructor(
+    public navCtrl: NavController,
+    public toastCtrl: ToastController
+  ) 
+  {
+    this.page = 2;
+
+  	this.WooCommerce = WC({
+  		url: 						"http://edwintrivinos.com/cusstom_apps/woo_commerce_ionic_3/",
+  		consumerKey: 		"ck_8f07070deceadfc7cbaffdb425b5621ef91e9956",
+  		consumerSecret: "cs_732111e679d2bd8563bec6c03d670a56680a26d2"
+  	});
+
+    this.loadMoreProducts(null);
+
+	  this.WooCommerce.getAsync("products")
+	  	.then((data) => {
+	  		this.products = JSON.parse(data.body).products;
+	  	}, (error) => {
+	  		console.log(error);
+	  	});
   }
 
+  ionViewDidLoad() {
+    setInterval(() => {
+      if(this.productSlides.getActiveIndex() === this.productSlides.length() - 1) {
+        this.productSlides.slideTo(0);
+      }
+      else {
+        this.productSlides.slideNext();
+      }
+    }, 3000);
+  }
+
+  loadMoreProducts(event) {
+    if(!event) { 
+      this.page = 2;
+      this.moreProducts = [];
+    }
+    else { this.page++; }
+
+    this.WooCommerce.getAsync("products?page=" + this.page)
+      .then((data) => {
+        let moreProducts = JSON.parse(data.body).products;
+        this.moreProducts = this.moreProducts.concat(moreProducts);
+
+        if(event) { 
+          event.complete(); 
+          if(moreProducts.length < 10) { 
+            event.enable(false); 
+
+            this.toastCtrl.create({
+              message: 'No more products!',
+              duration: 5000
+            }).present();
+          }
+        }
+      }, (error) => {
+        console.log(error);
+      });
+  }
 }
