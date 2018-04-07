6203b4a99696af514bcb458548c84e1dda0448a0
Edwin Alberto Triviños Ramírez <Edwin Alberto Trivios Ramrez>
2018-04-07 00:24:08 -0500

    Displaying Products in the Cart

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { Storage } from '@ionic/storage';
+
 @Component({
   selector: 'page-cart',
   templateUrl: 'cart.html',
 })
 export class CartPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+	cart: array[] = [];
+	total: any;
+
+  constructor(
+  	public navCtrl: NavController, 
+  	public navParams: NavParams,
+  	public storage: Storage
+	) {
+
+  	this.storage.ready().then(() => {
+	  	this.storage.get('cart').then((data) => {
+	      this.cart = data || [];
+
+	      this.total = this.cart.reduce((previous, current, index) => {
+      		return previous + current.amount;
+	      }, 0);
+	    });
+  	});
+  	
+  }
 }
